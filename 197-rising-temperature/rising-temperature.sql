e855f38ea9ba87fcdc0d9b9ace845871b60b4dbc
Anvay Srivastava <85279592+anvay1209@users.noreply.github.com>
2026-04-04 18:58:45 +0530

    Time: 485 ms (41.77%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,4 +1,4 @@
-SELECT t.id
-FROM Weather y
-CROSS JOIN Weather t
-WHERE DATEDIFF(t.recordDate,y.recordDate) = 1 AND t.temperature > y.temperature
+select t.id
+from Weather t
+cross join Weather y
+where DATEDIFF(t.recordDate, y.recordDate) = 1 AND t.temperature > y.temperature;
