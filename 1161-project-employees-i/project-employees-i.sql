f486a6cafc09693bf5751328265ff6894a3f9b19
Anvay Srivastava <85279592+anvay1209@users.noreply.github.com>
2026-04-05 00:50:19 +0530

    Time: 470 ms (83.58%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,12 @@
+# round(avg(),2) as average_years 
+SELECT project_id,round((y/x),2) as average_years 
+FROM (
+    SELECT 
+        p.project_id,
+        COUNT(*) AS x,
+        SUM(e.experience_years) AS y
+    FROM Project p
+    JOIN Employee e 
+        ON e.employee_id = p.employee_id
+    GROUP BY p.project_id
+) t;
