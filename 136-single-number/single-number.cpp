f9ef78ad20b4634df71f4e84ab1fe196cc7ed4c1
Anvay Srivastava <85279592+anvay1209@users.noreply.github.com>
2026-04-06 03:16:11 +0530

    Time: 10 ms (14.41%) | Memory: 23.9 MB (10.82%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_set<int> s;
+
+        for (int i : nums) {
+            if (s.find(i) != s.end()) s.erase(i);
+            else s.insert(i);
+            
+        }
+        return *s.begin();
+    }
+};
