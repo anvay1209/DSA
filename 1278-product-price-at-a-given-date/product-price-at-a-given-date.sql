d7e25bb1e5c06dccc5904070ccd2054890960798
Anvay Srivastava <85279592+anvay1209@users.noreply.github.com>
2026-04-08 14:41:06 +0530

    Time: 629 ms (27.93%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,13 +1,8 @@
-SELECT 
-    p.product_id,
-    IFNULL(
-        (
-            SELECT new_price
-            FROM Products p2
-            WHERE p2.product_id = p.product_id
-              AND p2.change_date <= '2019-08-16'
-            ORDER BY change_date DESC
-            LIMIT 1
-        ), 10
-    ) AS price
+SELECT p.product_id,IFNULL(
+        (SELECT new_price
+        FROM Products p2
+        WHERE p2.product_id = p.product_id AND p2.change_date <= '2019-08-16'
+        ORDER BY change_date DESC
+        LIMIT 1
+        ), 10) AS price
 FROM (SELECT DISTINCT product_id FROM Products) p;
